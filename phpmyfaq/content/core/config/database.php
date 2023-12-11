44f79110d44b21b9cedb9600ecb46c3ff28e85ea
Eziel Silva <slv.eziel@gmail.com>
2023-12-11 20:05:10 -0300

    database

--- phpmyfaq/content/core/config/database.php
+++ phpmyfaq/content/core/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$DB['server'] = '127.0.0.1';
+$DB['server'] = 'ihospitality-admin.cluster-cimut7x4nxvh.us-east-1.rds.amazonaws.com';
 $DB['port'] = '3306';
-$DB['user'] = 'root';
-$DB['password'] = '123a45b';
+$DB['user'] = 'ihospitality_aws';
+$DB['password'] = 'e6Fv920j%';
 $DB['db'] = 'ihospitality_faq';
 $DB['prefix'] = '';
 $DB['type'] = 'mysqli';
