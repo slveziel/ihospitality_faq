bc7111d0fd4cb9a08030611966b73696a81b6714
Eziel Silva <slv.eziel@gmail.com>
2023-12-11 19:36:41 -0300

    database

--- phpmyfaq/content/core/config/database.php
+++ phpmyfaq/content/core/config/database.php
@@ -1,8 +1,16 @@
 <?php
-$DB['server'] = 'ihospitality-origin.comets.com.br';
+$DB['server'] = '127.0.0.1';
 $DB['port'] = '3306';
-$DB['user'] = 'ihospitality_aws';
-$DB['password'] = 'e6Fv920j%';
+$DB['user'] = 'root';
+$DB['password'] = '123a45b';
 $DB['db'] = 'ihospitality_faq';
 $DB['prefix'] = '';
 $DB['type'] = 'mysqli';
+
+// $DB['server'] = '127.0.0.1';
+// $DB['port'] = '3306';
+// $DB['user'] = 'root';
+// $DB['password'] = '123a45b';
+// $DB['db'] = 'ihospitality_faq';
+// $DB['prefix'] = '';
+// $DB['type'] = 'mysqli';
