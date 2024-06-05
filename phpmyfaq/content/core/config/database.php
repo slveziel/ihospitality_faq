d130624970813e5422ee1e58851ddfb0de974ab0
Eziel Silva <slv.eziel@gmail.com>
2024-06-05 09:00:58 -0300

    Altera configuração de db

--- phpmyfaq/content/core/config/database.php
+++ phpmyfaq/content/core/config/database.php
@@ -6,3 +6,11 @@ $DB['password'] = 'e6Fv920j%';
 $DB['db'] = 'ihospitality_faq';
 $DB['prefix'] = '';
 $DB['type'] = 'mysqli';
+
+// $DB['server'] = 'localhost';
+// $DB['port'] = '3306';
+// $DB['user'] = 'root';
+// $DB['password'] = '123a45b';
+// $DB['db'] = 'ihospitality_faq';
+// $DB['prefix'] = '';
+// $DB['type'] = 'mysqli';
